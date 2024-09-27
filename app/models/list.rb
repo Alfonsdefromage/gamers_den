befeb114ab3faf69a93cc90283b10457bbe6ee77
Igor Zapala <zapala.igor@gmail.com>
2024-09-27 12:20:21 +0900

    started laying out the wishlist page

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,7 @@
 class List < ApplicationRecord
   belongs_to :user
   has_many :bookmarks, dependent: :destroy
-  has_many :games, through: :boookmarks
+  has_many :games, through: :bookmarks
 
   validates :name, presence: true
   validates :name, uniqueness: { scope: :user_id }
