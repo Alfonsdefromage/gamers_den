4206d71a8afb88945ab7261a9c35e4c411761cc1
Igor Zapala <zapala.igor@gmail.com>
2024-09-29 00:50:52 +0900

    started working on a form to update wishlist items

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,5 +2,17 @@ class ListsController < ApplicationController
 
   def wishlist
     @list = List.where(user: current_user).find_by(name: "Wishlist")
+    @bookmarks = Bookmark.where(list: @list)
+  end
+
+  def replace_button
+    @list = List.where(user: current_user).find_by(name: "Wishlist")
+    @game = Game.find(params[:game_id])
+    @bookmark = Bookmark.find_by(game_id: params[:game_id], list: @list)
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace("replace-#{params[:game_id]}", partial: "shared/wishlist_update", locals: { game: @game })
+      end
+    end
   end
 end
