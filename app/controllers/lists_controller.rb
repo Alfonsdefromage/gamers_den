9c84d96f7ea51b5bcce9189f0d2e9afed18c00a1
Igor Zapala <zapala.igor@gmail.com>
2024-10-11 00:26:28 +0900

    I've added a list showpage, where all owned games are displayed for each console, and I added a page called Pile of Shame which shows all games that the user owns, but haven't completed yet.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,11 +2,11 @@ class ListsController < ApplicationController
 
   def index
     @lists = List.all.where(user: current_user)
+    @bookmarks = Bookmark.all.where(user: current_user)
   end
 
   def show
     @list = List.find(params[:id])
     @bookmarks = Bookmark.all.where(user: current_user, platform: @list.name)
   end
-
 end
